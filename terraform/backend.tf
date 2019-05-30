ed4fceab232c0592ecf2f3d810020a33bfe16bcf
Joey Lorich <jolorich@microsoft.com>
2019-05-30 17:51:43 -0600

    initial commit

--- terraform/backend.tf
+++
@@ -1,6 +0,0 @@
-terraform {
-  backend "azurerm" {
-    container_name = "tfstate"
-    key            = "demo-aks.tfstate"
-  }
-}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,18 +8,7 @@ variable "name" {
 variable "environment" {
   type        = "string"
   description = "Name of the deployment environment"
-  default     = "sandbox"
-}
-
-// Service Principal
-variable "client_id" {
-  type        = "string"
-  description = "Service Principal Client ID"
-}
-
-variable "client_secret" {
-  type        = "string"
-  description = "Service Principal Client Secret"
+  default     = "dev"
 }
 
 // Resource information
@@ -64,4 +53,43 @@ variable "dns_prefix" {
   type        = "string"
   description = "DNS Prefix"
   default     = "mtcden"
+}
+
+// Network information
+
+variable "vnet_address_space" {
+  type        = "string"
+  description = "Address space for the vnet"
+  default     = "10.0.0.0/8"
+}
+
+variable "vnet_aks_subnet_space" {
+  type        = "string"
+  description = "Address space for the AKS subnet"
+  default     = "10.1.0.0/16"
+}
+
+variable "vnet_ingress_subnet_space" {
+  type        = "string"
+  description = "Address space for the gateway subnet"
+  default     = "10.2.0.0/24"
+}
+
+variable "vnet_gateway_subnet_space" {
+  type        = "string"
+  description = "Address space for the gateway subnet"
+  default     = "10.2.1.0/24"
+}
+
+variable "ingress_load_balancer_ip" {
+  type        = "string"
+  description = "Address for the ingress controller load balancer"
+  default     = "10.2.0.10"
+}
+
+
+variable "gateway_instance_count" {
+  type        = "string"
+  description = "The number of application gateways to deploy"
+  default     = "1"
 }
